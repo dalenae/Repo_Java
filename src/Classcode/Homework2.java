c48611af5ba8588f2864b27633b34a7a2d588b00
Dalena Eyasu <Dalena@Dalenas-MacBook-Air.local>
2022-03-07 19:25:42 -0600

    Homework 2

---
+++ src/Classcode/Homework2.java
@@ -0,0 +1,28 @@
+package Classcode;
+
+public class Homework2 {
+    public static void main(String[] args) {byte numberOfHoursInDay = 24;
+        System.out.println("Number of hours in a day -> " + numberOfHoursInDay);
+
+        short maxNumberOfDaysInYear = 365;
+        System.out.println("Max number of days in a year -> " + maxNumberOfDaysInYear);
+
+        byte totalNumberOfEmployees = 127;
+        System.out.println("Total number of employees -> " + totalNumberOfEmployees);
+
+        int unitedStatesPopulation = 331002651;
+        System.out.println("United states population -> " + unitedStatesPopulation);
+
+        float mortgageRate = 2.49f;
+        System.out.println("Mortgage Rate -> " + mortgageRate);
+
+
+
+
+
+
+
+
+
+    }
+}
